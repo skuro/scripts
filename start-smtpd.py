b0c76cff126dfd85d38a1880920664d81c8a1e05
Carlo Sciolla <carlo.sciolla@gmail.com>
2010-09-10 11:34:57 +0100

    Updated to leiningen v1.3.0, added an echo SMTP server in python

---
+++ start-smtpd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import smtpd
+import asyncore
+import sys
+
+class CustomSMTPServer(smtpd.SMTPServer):
+    
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        print 'Receiving message from:', peer
+        print 'Message addressed from:', mailfrom
+        print 'Message addressed to  :', rcpttos
+        print 'Message length        :', len(data)
+        return
+
+print "Starting the echo SMTP server"
+
+port = 1025
+if len(sys.argv) > 1:
+    try:
+        port = int(sys.argv[1])
+    except ValueError:
+        print sys.argv[1] +" is not a valid port number, defaulting to 1025"
+        port = 1025
+
+server = CustomSMTPServer(('127.0.0.1', port), None)
+
+asyncore.loop()
