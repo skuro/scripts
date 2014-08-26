a85048f08912408f185a559ee50d004fe0cc2c60
Carlo Sciolla <skuro@skuro.tk>
2014-08-26 22:25:34 +0200

    v1.0 of the alfresco deployer

--- alfresco-m2.sh
+++ alfresco-m2.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+##########################################################
+#
+# Automatic deployment of your Alfresco artifacts
+#
+# Requires the following file structure:
+#
+# ${BASE_DIR}/alfresco.war
+# ${BASE_DIR}/alfresco/WEB-INF/lib/...
+# ${BASE_DIR}/share.war
+# ${BASE_DIR}/share/WEB-INF/lib/...
+#
+# @author  Carlo Sciolla <skuro@skuro.tk>
+# @version 1.0
+##########################################################
 
 ##################### User inputs ########################
 
 # The version of the alfresco WAR file you're uploading
-VERSION=${1:-VERSION_UNSET}
+ALF_VERSION=${1:-VERSION_UNSET}
 
 # The ID of the remote repository, to match with your maven settings
 TARGET_REPO=${2:-REPO_UNSET}
@@ -33,111 +47,118 @@ else
     GROUP_ID="org.alfresco"
 fi
 
-# TODO: find the files directly on the file system instead of hard coding them
-ARTIFACTS=(
-    alfresco-core
-    alfresco-data-model
-    alfresco-deployment
-    alfresco-enterprise-remote-api
-    alfresco-enterprise-repo
-    alfresco-jlan-embed
-    alfresco-mbeans
-    alfresco-remote-api
-    alfresco-repository
-    #alfresco-wdr-deployment.jar: WTF?
-    alfresco-web-client
-    alfresco-web-framework-commons
-)
+##
+## Used to track what artifacts were deployed by this program execution
+##
+DEPLOYED_SO_FAR=()
 
-# Some artifacts don't respect alfresco versioning
-EXCEPTIONS=(
-    alfresco-xmlfactory:1.0.1
-)
-
-# Some artifacts don't have a version attached at all
-NOVERSION=(
-    alfresco-wdr-deployment:jar
-    alfresco:war
-)
-
-function file_noversion(){
-    ARTIFACT_ID=${1}
-    PACKAGING=${2:-jar}
-
-    if [ ${PACKAGING} == "jar" ]
-    then
-        FOLDER="${BASE_DIR}/alfresco/WEB-INF/lib"
-    else
-        FOLDER="${BASE_DIR}"
-    fi
-
-    FILE="${FOLDER}/${ARTIFACT_ID}.${PACKAGING}"
+##
+## Checks whether an element is found in an array
+## Curtesy of http://stackoverflow.com/questions/3685970/check-if-an-array-contains-a-value
+##
+contains_element () {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
 }
 
+##
+## Checks whether the given artifacts was already deployed during this program execution
+##
+function already_deployed(){
+    ARTIFACT=${1}
+    contains_element "${ARTIFACT}" "${DEPLOYED_SO_FAR[@]}"
+}
+
+##
+## Deploys an artifact via maven using the provided coordinates and repo config.
+## If a file was already deployed during this program execution it will be skipped.
+##
 function deploy(){
-    ARTIFACT_ID=${1}
-    PACKAGING=${2:-jar}
-    FILE=${3:-${BASE_DIR}/alfresco/WEB-INF/lib/${ARTIFACT_ID}-${VERSION}.jar}
+    local ARTIFACT_ID=${1}
+    local PACKAGING=${2:-jar}
+    local VERSION=${3:-${ALF_VERSION}}
+    local FILE=${4:-${BASE_DIR}/alfresco/WEB-INF/lib/${ARTIFACT_ID}-${VERSION}.jar}
 
-    echo -e -n "\tDeploying ${CYAN}${ARTIFACT_ID}${NOCOLOR}"
-
-    OUTPUT=$(mvn deploy:deploy-file \
-        -Dfile="${FILE}" \
-        -DrepositoryId="${TARGET_REPO}" \
-        -DgroupId="${GROUP_ID}" \
-        -DartifactId="${ARTIFACT_ID}" \
-        -Dversion="${VERSION}" \
-        -Durl="${TARGET_REPO_URL}" \
-        -Dpackaging="${PACKAGING}"  \
-        -Dclassifier="${RELEASE}" 2>&1)
-
-    if [ ${?} -eq 0 ]
+    already_deployed ${ARTIFACT_ID} > /dev/null 2>&1
+    local IS_DEPLOYED=${?}
+    if [[ ${IS_DEPLOYED} -eq 0 ]]
     then
-        echo -e "${SPACER}${GREEN}OK${NOCOLOR}"
+        : # skipping an already deployed artifact
+    elif [[ -f ${FILE} ]]
+    then
+        echo -e -n "\tDeploying ${CYAN}${ARTIFACT_ID}${NOCOLOR}"
+
+        local OUTPUT=$(mvn deploy:deploy-file \
+            -Dfile="${FILE}" \
+            -DrepositoryId="${TARGET_REPO}" \
+            -DgroupId="${GROUP_ID}" \
+            -DartifactId="${ARTIFACT_ID}" \
+            -Dversion="${VERSION}" \
+            -Durl="${TARGET_REPO_URL}" \
+            -Dpackaging="${PACKAGING}"  \
+            -Dclassifier="${RELEASE}" 2>&1)
+
+        if [ ${?} -eq 0 ]
+        then
+            echo -e "${SPACER}${GREEN}OK${NOCOLOR}"
+            DEPLOYED_SO_FAR+=(${ARTIFACT_ID})
+        else
+            echo -e "${SPACER}${RED}FAIL${NOCOLOR}"
+            echo -e "Maven output:"
+            echo -e ${OUTPUT}
+        fi
     else
-        echo -e "${SPACER}${RED}FAIL${NOCOLOR}"
-        echo -e "Maven output:"
-        echo -e ${OUTPUT}
+        echo -e "${RED}File not found: ${FILE}${NOCOLOR}"
     fi
 }
 
-function parse_exception(){
-    BACKUP=$IFS
-    IFS=":"
-    read -ra PARTS <<< "$1"
-    IFS=${BACKUP}
+##
+## Discovers inner alfresco-* JARs inside a given exploded APP and deploys them.
+## It recognizes both the following naming formats:
+## - alfresco-[my-lib-name]-x.x.x.x.jar
+## - alfresco-[my-lib-with-no-version].jar
+##
+function deploy_libraries(){
+    local APP=${1}
 
-    ARTF=${PARTS[0]}
-    VERSION=${PARTS[1]}
+    local FULL_REGEX="(alfresco-[a-zA-Z\-]+)-([0-9\.]+).jar"
+    local NOVERSION_REGEX="(alfresco-[a-zA-Z\-]+).jar"
+    for LIB_PATH in `ls ${BASE_DIR}/${APP}/WEB-INF/lib/alfresco*`
+    do
+        local LIB=`basename $LIB_PATH`
+        local VERSION=${ALF_VERSION}
+        if [[ $LIB =~ $FULL_REGEX ]]
+        then
+            local VERSION=${BASH_REMATCH[2]}
+        elif [[ $LIB =~ ${NOVERSION_REGEX} ]]
+        then
+            : # NOOP, just needed to run the regex to populate ${BASH_REMATCH[]}
+        fi
+        deploy ${BASH_REMATCH[1]} "jar" ${VERSION} ${LIB_PATH}
+    done
 }
 
-function parse_noversion(){
-    BACKUP=$IFS
-    IFS=":"
-    read -ra PARTS <<< "$1"
-    IFS=${BACKUP}
-
-    ARTF=${PARTS[0]}
-    PACKAGING=${PARTS[1]}
+##
+## Capitalizes the first letter of the provided token
+##
+function uppercase(){
+    TOKEN="${1}"
+    echo $(echo ${TOKEN:0:1} | tr '[a-z]' '[A-Z]')${TOKEN:1}
 }
 
-echo -e "Uploading ${CYAN}Alfresco v${VERSION}${NOCOLOR} JARs to repo ${TARGET_REPO} at ${TARGET_REPO_URL}"
+##
+## Deploys a WAR file and its inner alfresco-* JARs
+##
+function deploy_app(){
+    local APP=${1}
+    local APP_CAPITAL=$(uppercase ${APP})
+    echo -e "Uploading ${CYAN}${APP_CAPITAL} v${ALF_VERSION}${NOCOLOR} artifacts to repo ${TARGET_REPO} at ${TARGET_REPO_URL}"
+    deploy_libraries ${APP}
+    deploy ${APP} "war" ${ALF_VERSION} "${APP}.war"
+}
 
-echo
-for ARTF in ${ARTIFACTS[@]}
+for APP in alfresco share
 do
-    deploy ${ARTF}
-done
-
-for ARTF in ${EXCEPTIONS[@]}
-do
-    parse_exception "$ARTF"
-    deploy ${ARTF}
-done
-
-for ARTF in ${NOVERSION[@]}
-do
-    parse_noversion ${ARTF}
-    file_noversion ${ARTF} ${PACKAGING}
-    deploy ${ARTF} ${PACKAGING} ${FILE}
+    deploy_app ${APP}
 done
